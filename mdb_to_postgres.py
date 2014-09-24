b13868e08d9509a48b40b31968bac46a19271658
Patrick Sanders <mail@patricksanders.net>
2014-09-24 15:18:25 -0500

    improved file uploading using jQuery FileUpload

--- mdb_to_postgres.py
+++ mdb_to_postgres.py
@@ -8,38 +8,49 @@ ALLOWED_EXTENSIONS = set(['mdb'])
 app = Flask(__name__)
 app.config.from_object('default_settings')
 
-def allowed_file(filename):
-	return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	if request.method == 'POST':
-		db_file = request.files['file']
-		db_user = request.values.get('inputUser')
-		db_user_password = request.values.get('inputUserPassword')
-		db_admin_password = request.values.get('inputAdminPassword')
-		if db_file and allowed_file(db_file.filename):
-			filename = secure_filename(db_file.filename)
-			file_path = os.path.join(app.config['WORKING_FOLDER'], filename)
-			db_file.save(file_path)
-			try:
-				result, detail = start_import(file_path, db_user, db_user_password, db_admin_password)
-				return render_template('result.html',
-									   p=app.config['TEMPLATE_PARAMS'],
-									   result=result,
-									   detail=detail)
-			except Exception as e:
-				friendly, unfriendly = e.args
-				return render_template('error.html',
-									   error=friendly,
-									   description=unfriendly,
-									   p=app.config['TEMPLATE_PARAMS'])
-	return render_template('index.html',
+	return render_template('form.html',
 						   p=app.config['TEMPLATE_PARAMS'])
+
+@app.route('/submit', methods=['POST'])
+def submit():
+	raw_filename = request.values.get('inputDbFile')
+	db_filename = secure_filename(raw_filename)
+	db_user = request.values.get('inputUser')
+	db_user_password = request.values.get('inputUserPassword')
+	db_admin_password = request.values.get('inputAdminPassword')
+	try:
+		result, detail = start_import(db_filename, db_user, db_user_password, db_admin_password)
+		return render_template('result.html',
+							   p=app.config['TEMPLATE_PARAMS'],
+							   result=result,
+							   detail=detail)
+	except Exception as e:
+		#friendly, unfriendly = e.args
+		unfriendly = e.args
+		return render_template('error.html',
+							   #error=friendly,
+							   description=unfriendly,
+							   p=app.config['TEMPLATE_PARAMS'])
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
 	return send_from_directory(app.config['WORKING_FOLDER'], filename)
+
+@app.route('/upload', methods=['POST'])
+def upload():
+	db_file = request.files['file']
+	if db_file and allowed_file(db_file.filename):
+		filename = secure_filename(db_file.filename)
+		file_path = os.path.join(app.config['WORKING_FOLDER'], filename)
+		db_file.save(file_path)
+		return 'yep', 200
+	else:
+		return 'nope', 500
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 def start_import(db_file, db_user, db_user_password, db_admin_password):
 	importer = import_mdb(db_file, db_user, db_user_password, db_admin_password)
