958f3af4f5cd0e8024ee66ee2d608eefa75a9b9c
Patrick Sanders <mail@patricksanders.net>
2014-09-29 12:53:29 -0500

    send inserts directly to psycopg instead of writing to file

--- import_mdb.py
+++ import_mdb.py
@@ -32,6 +32,7 @@ class import_mdb:
 	log_output = ''
 
 	def __init__(self, mdb_path, db_user_username, db_user_password, db_admin_password, db_admin_username, db_host, db_port, working_dir):
+		# assign values to instance variables
 		self._mdb_path = os.path.abspath(os.path.join(working_dir, mdb_path))
 		self._user_password = db_user_password
 		self._database_user = db_user_username
@@ -41,11 +42,23 @@ class import_mdb:
 		self._database_port = db_port
 		self._working_dir = working_dir
 	
-	def dump(self):
-		self.log('Schema file => ' + str(self.schema_sql_filename))
-		self.log('Table files => ' + str(self.table_sql_filenames))
-	
+	def dump_tables_to_db(self, tables, cursor):
+		'''Dump each table in mdb to sql file'''
+		table_files = []
+		for table in tables:
+			if table != '':
+				filename = os.path.abspath(os.path.join(self._working_dir, 'table_' + table.lower() + '.sql'))
+				table_files = table_files + [filename]
+				self.log('Dumping ' + table + ' table...')
+				command = ['mdb-export', '-I', 'postgres', '-q', "'", self._mdb_path, table.lower()]
+				insert_statements = subprocess.Popen(command, stdout=subprocess.PIPE).communicate()[0].strip().split('\n')
+				[self.run_insert(line, cursor) for line in insert_statements]
+
 	def get_replacements(self):
+		'''Table and column names need to be replace with lowercase
+		to avoid problems with case sensitivity in postgres. This method
+		compiles all table and column names into a list.
+		'''
 		replacements = []
 		tables = self.get_table_names()
 		for table in tables:
@@ -72,9 +85,7 @@ class import_mdb:
 		db_name = os.path.split(mdb_path)[1].strip('.mdb')
 		self._database_name = db_name.lower()
 		self._replacements = self._replacements + self.get_replacements()
-		database_name = self._database_name
-		database_user = self._database_user
-		self.schema_sql_filename = self.write_schema_to_sql(db_name)
+		self.schema_sql_filename = self.write_schema_to_sql()
 
 		try:
 			con = psycopg2.connect(dbname='postgres',
@@ -163,57 +174,53 @@ class import_mdb:
 	def log(self, text):
 		self.log_output = self.log_output + text + '\n'
 		
-	def replace_with_lower(self, text, terms):
-		'''Replace all instances of a list of words with the lowercase of each word'''
-		for term in terms:
-			expression = re.compile(re.escape(term), re.IGNORECASE)
-			text = expression.sub(term.lower(), text)
-			self.log(term + ' replaced with ' + term.lower())
-		return text
+	def cleanup_schema(self, text, terms):
+		'''Replace all instances of a list of words with the
+		lowercase of each word
+		'''
+		if text.startswith('ALTER TABLE') or text.startswith('CREATE INDEX') or text.startswith('CREATE UNIQUE INDEX'):
+			return '\n'
+		else:
+			for term in terms:
+				# find and replace term with lowercase
+				expression = re.compile(re.escape(term), re.IGNORECASE)
+				text = expression.sub(term.lower(), text)
+				self.log(term + ' replaced with ' + term.lower())
+			return text
 	
 	def run_insert(self, text, cursor):
+		'''Prepare and run each insert statement
+		Preparation is for case correction
+		'''
+		# regex for converting table and field names to lowercase
 		expression = re.compile('^INSERT INTO "([a-z]+)" \((.*)\) VALUES')
+		# replace table name with lower
 		text = re.sub(expression, lambda x: x.group(0).lower(), text)
+		# replace field name with lower
 		text = re.sub(expression, lambda x: x.group(1).lower(), text)
 		try:
+			# execute insert statement
 			cursor.execute(text)
-			self.log('ran insert: ' + text)
+			#self.log('ran insert: ' + text)
 		except psycopg2.ProgrammingError as e:
+			# some sort of syntax error
 			self.log('Uh oh! ' + str(e))
 			self.log(traceback.format_exc())
 		except psycopg2.IntegrityError as e:
+			# thrown if there's a problem with duplicate primary keys or other constraints
 			self.log('Uh oh! ' + str(e))
 			self.log(traceback.format_exc())
 	
-	def write_schema_to_sql(self, db_name):
-		schema_file = os.path.abspath(os.path.join(self._working_dir, 'schema_' + db_name.lower() + '.sql'))
+	def write_schema_to_sql(self):
+		schema_file = os.path.abspath(os.path.join(self._working_dir, 'schema_' + self._database_name + '.sql'))
 		with open(schema_file, 'w') as f:
 			self.log('Extracting schema...')
 			schema = subprocess.Popen(['mdb-schema', self._mdb_path, 'postgres'],
 									  stdout=subprocess.PIPE).communicate()[0]
-			schema = self.replace_with_lower(schema, self._replacements)
+			schema = self.cleanup_schema(schema, self._replacements)
 			expression = re.compile(re.escape('BOOL'))
 			schema = expression.sub('INTEGER', schema)
 			f.write(schema)
 			self.log('Schema dumped to ' + schema_file)
 		return schema_file
-
-	def dump_tables_to_db(self, tables, cursor):
-		'''Dump each table in mdb to sql file'''
-		table_files = []
-		for table in tables:
-			if table != '':
-				filename = os.path.abspath(os.path.join(self._working_dir, 'table_' + table.lower() + '.sql'))
-				table_files = table_files + [filename]
-				self.log('Dumping ' + table + ' table...')
-				command = ['mdb-export', '-I', 'postgres', '-q', "'", self._mdb_path, table.lower()]
-				insert_statements = subprocess.Popen(command, stdout=subprocess.PIPE).communicate()[0].strip().split('\n')
-				[self.run_insert(line, cursor) for line in insert_statements]
-
-if __name__ == '__main__':
-	print 'Welcome to import_mdb'
-	importer = import_mdb(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
-	print 'Starting dump...'
-	schema_file, table_files = importer.dump()
-	database_name, database_user = importer.import_db(schema_file, table_files)
 
