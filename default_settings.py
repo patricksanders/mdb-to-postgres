dfcc02c9b3d755fb689ec048fecbed6be5da2d84
Patrick Sanders <mail@patricksanders.net>
2016-02-22 20:27:53 -0600

    add new default settings file

---
+++ default_settings.py
@@ -0,0 +1,9 @@
+DEBUG = True
+WORKING_FOLDER = './working'
+TEMPLATE_PARAMS = {'site_title': 'MDB to PostgreSQL Importer',
+				   'site_url': 'http://localhost:5000',
+				   'links': [{'name': 'Other Webapp', 'url': 'http://localhost/'},
+				             {'name': 'And Another', 'url': 'https://google.com/'}],
+				   'credit': {'name': 'Patrick Sanders', 'url': 'http://patricksanders.net/'}
+				  }
+
