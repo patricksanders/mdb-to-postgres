8859a36211e085019f2da2e30616bac73a607ee1
Patrick Sanders <mail@patricksanders.net>
2016-02-28 00:10:11 -0600

    change generic link titles

--- default_settings.py
+++ default_settings.py
@@ -2,9 +2,9 @@ DEBUG = True
 WORKING_FOLDER = './working'
 TEMPLATE_PARAMS = {'site_title': 'MDB to PostgreSQL Importer',
                    'site_url': 'http://localhost:5000',
-                   'links': [{'name': 'Other Webapp',
+                   'links': [{'name': 'Link 1',
                               'url': 'http://localhost/'},
-                             {'name': 'And Another',
+                             {'name': 'Link 2',
                               'url': 'https://google.com/'}],
                    'credit': {'name': 'Patrick Sanders',
                               'url': 'http://patricksanders.net/'}}
